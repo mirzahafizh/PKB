58dae5a573a40c6084038dbe9c63e3aa916c9678
Mirza <127849850+mirzahafizh@users.noreply.github.com>
2023-11-25 17:28:19 +0800

    Update PKB.py

--- PKB.py
+++ PKB.py
@@ -1,14 +1,15 @@
+import random
 import tkinter as tk
+import time
 
 
 class NQueensGame:
-    def __init__(self, master, n=4):
+    def __init__(self, master, n=8):
         self.master = master
         self.n = n
         self.master.title("N-Queens Game")
 
-        # Set a fixed initial state
-        self.board = [1, 2, 3, 0]
+        self.board = [1, 0, 2, 6, 4, 7, 5, 3]
 
         self.label = tk.Label(master, text="N-Queens Game")
         self.label.pack()
@@ -30,23 +31,35 @@ class NQueensGame:
         for i in range(self.n):
             for j in range(self.n):
                 color = "white" if (i + j) % 2 == 0 else "black"
-                self.canvas.create_rectangle(j * self.canvas_size, i * self.canvas_size, (j + 1) * self.canvas_size, (i + 1) * self.canvas_size, fill=color)
+                self.canvas.create_rectangle(j * self.canvas_size, i * self.canvas_size, (j + 1) * self.canvas_size,
+                                             (i + 1) * self.canvas_size, fill=color)
                 if j == self.board[i]:
                     self.canvas.create_text((j + 0.5) * self.canvas_size, (i + 0.5) * self.canvas_size,
                                             text="Q", font=("Helvetica", 12), fill="red")
 
     def solve(self):
+        # Mulai mengukur waktu eksekusi
+        start_time = time.time()
+
         solution = self.hill_climbing()
+
+        end_time = time.time()
+        execution_time = end_time - start_time
+
         if solution:
             self.board = solution
             self.draw_board()
 
+        print(f"Waktu Eksekusi solve(): {execution_time} detik")
+
     def reset(self):
-        self.board = [1,2,3,0]  # Reset to the fixed initial state
+        #self.board = self.initial_solution()
+        self.board = [1, 0, 2, 6, 4, 7, 5, 3]
         self.draw_board()
 
     def initial_solution(self):
-        return [1, 2,3,0]  # Set a fixed initial state
+        #return random.sample(range(self.n), self.n)  # Solusi awal dengan konfigurasi acak
+        return [1, 0, 2, 6, 4, 7, 5, 3]
 
     def calculate_attacks(self, queens):
         attacks = 0
@@ -57,12 +70,20 @@ class NQueensGame:
         return attacks
 
     def hill_climbing(self):
-        iterations = 1000
+        # Mulai mengukur waktu eksekusi
+        start_time = time.time()
+
+        iterations = 100
 
         for _ in range(iterations):
             current_attacks = self.calculate_attacks(self.board)
             if current_attacks == 0:
-                return self.board  # Solution found
+                # Selesai mengukur waktu eksekusi
+                end_time = time.time()
+                execution_time = end_time - start_time
+                print(f"Waktu Eksekusi hill_climbing(): {execution_time} detik")
+
+                return self.board  # Solusi ditemukan
 
             for i in range(self.n):
                 for j in range(self.n):
@@ -73,14 +94,21 @@ class NQueensGame:
 
                         if new_attacks < current_attacks:
                             self.board = temp_queens
-                            return temp_queens  # Choose the first step that reduces the number of attacks
+                            return temp_queens  # Memilih langkah pertama yang mengurangi jumlah serangan
 
-        return None  # Solution not found within the iteration limit
+        # Selesai mengukur waktu eksekusi di luar loop
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"Waktu Eksekusi hill_climbing(): {execution_time} detik")
+
+        return None  # Solusi tidak ditemukan dalam batas iterasi
+
 
 def main():
     root = tk.Tk()
-    game = NQueensGame(root, n=4)
+    game = NQueensGame(root, n=8)
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
